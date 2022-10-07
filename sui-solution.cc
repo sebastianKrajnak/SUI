b81d758ac7725cea702bc53469af4de0479bad05
sebastianKrajnak <sebastian.krajnak@gmail.com>
2022-10-07 22:14:57 +0200

    started working on backtrace from goaL NOT WORKING

--- sui-solution.cc
+++ sui-solution.cc
@@ -2,57 +2,44 @@
 #include "memusage.h"
 #include <deque>
 #include <map>
+#include <memory>
 #include <set>
 #include <tuple>
 
 int memory_limit = 100 * (1024^2);
 
-std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
-	/* std::vector<SearchAction> solution;
-	std::deque<SearchState> open;
-	std::set<SearchState> closed;
-	std::map< std::shared_ptr<SearchState>, std::tuple<std::shared_ptr<SearchState>, SearchAction> > action_map; 
+std::vector<SearchAction> FindSolution(const SearchState &init_state, SearchState &goal, std::map< SearchState, std::tuple<SearchState, SearchAction> > action_map){
+	auto tmp_iterator = action_map.find(goal);
+	auto tmp_tuple = tmp_iterator->second;
+	auto tmp_state = std::get<0>(tmp_tuple);
+	auto tmp_sol_move = std::get<1>(tmp_tuple);
+	
+	std::vector<SearchAction> solution;
+	solution.push_back(tmp_sol_move);
 
-	open.push_back(init_state);
-
-	while(!open.empty()){
-		if(getCurrentRSS() >= mem_limit_ - memory_limit){
-			std::cout << "Hit mem limit" << std::endl;
-			break;
+	while(true){
+		if(init_state == tmp_state){
+			return solution;
 		}
-		
-		SearchState working_state(open.front());
-		open.pop_front();
-		auto actions = working_state.actions();
-		
-		for( auto &action : actions){
-			SearchState new_state(working_state);
-			new_state = action.execute(new_state);
-			if( closed.find(*working_state) == closed.end()){	//current working_state is NOT in closed
-				closed.emplace(
-					std::make_shared<SearchState>(new_state), 
-					std::make_tuple(std::make_shared<SearchState>(working_state), action)
-				);
-				if( closed.find(new_state) == closed.end() ){
-					solution.push_back(action);
-					if(new_state.isFinal()){
-						std::cout << "Found solution!" << std::endl;
-						return solution;
-					}
-					open.push_back(new_state);
-				}
-			}
+		else{
+			tmp_iterator = action_map.find(tmp_state);
+			tmp_tuple = tmp_iterator->second;
+			tmp_state = std::get<0>(tmp_tuple);
+			tmp_sol_move = std::get<1>(tmp_tuple);
+
+			solution.push_back(tmp_sol_move);
 		}
 	}
-	return {};
-}  */
+}
+
+std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
 
 	std::vector<SearchAction> solution;
 	std::deque<SearchState> open;
 	std::set<SearchState> closed; 
 	// where first state is new_state and the tuple consists of parent_state and action that lead from parent to new
-	std::map< std::shared_ptr<SearchState>, std::tuple<std::shared_ptr<SearchState>, SearchAction> > action_map;
-
+	//std::map< std::shared_ptr<SearchState>, std::tuple<std::shared_ptr<SearchState>, SearchAction> > action_map;
+	std::map< SearchState, std::tuple<SearchState, SearchAction> > action_map;
 
 	open.push_back(init_state);
 
@@ -75,14 +62,24 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 			for( auto &action : actions){
 				SearchState new_state(working_state);
 				new_state = action.execute(new_state);
+				/* auto state_ptr = std::make_shared<SearchState>(new_state);
 				action_map.emplace(
-					std::make_shared<SearchState>(new_state),
+					state_ptr,
 					std::make_tuple(std::make_shared<SearchState>(working_state), action)
-					);
+					); */
+				action_map.emplace(
+					new_state,
+					std::make_tuple(working_state, action));
+ 
 				if(new_state.isFinal()){
-					
+					auto tmp_iterator = action_map.find(new_state);
+					auto tmp_tuple = tmp_iterator->second;
+					auto tmp_state = std::get<0>(tmp_tuple);
+					auto tmp_sol_move = std::get<1>(tmp_tuple);
+
 					solution.push_back(action);
 					std::cout << "Found solution!" << std::endl;
+					std::cout << "Parent is: " << tmp_state << " and move is: " << tmp_sol_move << std::endl;
 					return solution;
 				}
 				open.push_back(new_state);
