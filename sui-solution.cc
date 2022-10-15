0315d33b243ec8b8a02be9fbe32252629b7c533e
sebastianKrajnak <sebastian.krajnak@gmail.com>
2022-10-15 18:02:42 +0200

    fixed a_star and HSDH both work now

--- sui-solution.cc
+++ sui-solution.cc
@@ -6,10 +6,17 @@
 #include <set>
 #include <tuple>
 #include <algorithm>
+#include <iterator>
 
 typedef std::pair<double, SearchState> Pair;
 
 int memory_threshold = 100 * (1024^2);
+
+struct compare{
+	bool operator()(const std::pair<double, SearchState>& a, const std::pair<double, SearchState>& b){
+		return a.first>b.first;
+	}
+};
 
 std::vector<SearchAction> FindSolution(
 	std::map<SearchState, std::tuple<std::shared_ptr<SearchState>, SearchAction>>::iterator &map_itr,
@@ -144,25 +151,6 @@ std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state)
 
 double StudentHeuristic::distanceLowerBound(const GameState &state) const {
 	// Heuristics src: https://www.human-competitive.org/sites/default/files/elyasaf-hauptmann-sipper-paper.pdf
-	/* 
-	SumOfBottomCards. Take the highest possible sum of
-	cards in the bottom of cascades (e.g. 100), and subtract the sum of values
-	of cards actually located there.
-	 */
-
-	/* double actual_sum = 0;
-	for (const auto &cascade : state.stacks) {
-        auto top_card = cascade.topCard();
-        if (top_card.has_value())
-            actual_sum += top_card->value;
-    }
-	 return 100 - actual_sum; */
-
-	/*  
-	DifferenceFromTop. The average value of the top cards
-	in cascades, minus the average value of the top cards in
-	home piles.
-	*/
 
 	double cascade_sum = 0;
 	double home_sum = 0;
@@ -180,7 +168,7 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
     }
 
 	 return cascade_sum/8 - home_sum/4;
-
+ 
 	/*
 	 Heineman’s Staged Deepening Heusirtic (HSDH). This is
 	the heuristic used by the HSD algorithm: For each home pile, locate within the cascade piles the next
@@ -188,96 +176,50 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
 	this count for all foundations. This number is multiplied by 2 if there are no free FreeCells or empty 
 	foundation piles (reflecting the fact that freeing the next card is harder in this case).
 	*/
-	
-	/* /for (const auto& home : state.homes) {
+	/* 
+	// works but is worse than the other one ? Should be better tho
+	double total_sum = 0;
+	int cards_ahead;
+
+	for (const auto& home : state.homes) {
 		auto home_card = home.topCard();
-		if(home_card.has_value())
-			auto home_card_value = home_card->value;
-		
-		std::cout << "Home card is: " << std::endl;
-		for (const auto& cascade : state.stacks) {
-			
-			 // I don't fuken kno how to get a list of all of card so I can compare them
-		} 
+		if(home_card.has_value()){
+			for (const auto& cascade : state.stacks) {
+				auto cascade_cards = cascade.storage();
+				auto it = std::find(cascade_cards.begin(), cascade_cards.end(), home_card);
+
+				if(  it != cascade_cards.end() ){
+					cards_ahead = std::distance(it, cascade_cards.end());
+					total_sum += cards_ahead;
+				}
+			} 
+		}
 	}
-	return 0; */
+	if(state.free_cells.size() == 4 || state.stacks.size() == 8){
+		return total_sum * 2;
+	}
+	else{
+		return total_sum;
+	} */
 }
 
 std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
-	//this works but not necessarily a_star
-	/* std::vector<SearchAction> solution;
-	std::map<double, SearchState> open;
-	std::set<SearchState> closed;
-	std::map<SearchState, std::tuple<std::shared_ptr<SearchState>, SearchAction> > action_map;
-	double cost = 0;	// g(n) part of the f(n) to calculate cost, ie. trips from source
-
-	if (init_state.isFinal())
-		return {};
-
-	auto state_ptr = std::make_shared<SearchState>(init_state);
-	action_map.emplace(init_state, std::make_tuple(nullptr, init_state.actions()[0]));
-	open.emplace(cost, init_state);
-
-	while(!open.empty()){
-		if(getCurrentRSS() >= mem_limit_ - memory_threshold){
-			std::cout << "Out of memory." << std::endl;
-			break;
-		}
-
-		SearchState working_state(open.begin()->second);
-		open.erase(open.begin());
-		auto search = closed.find(working_state);
-		auto actions = working_state.actions();
-		
-		if (search == closed.end()){	//std::maps end() fn goes through the entire map and returns past-the-end if key is not in map
-			closed.insert(working_state);
-			if (state_ptr == nullptr)
-				state_ptr = std::make_shared<SearchState>(working_state);
-
-			for( auto &action : actions){
-				SearchState new_state(working_state);
-				new_state = action.execute(new_state);
-				
-				auto h_n = compute_heuristic(new_state, *heuristic_);
-				auto f_n = cost + h_n;
-
-				action_map.emplace(new_state, std::make_tuple(state_ptr, action));
-				open.emplace(f_n, new_state);
-
-				if(new_state.isFinal()){
-					auto map_itr = action_map.find(new_state);
-					auto prev_ptr = std::shared_ptr<SearchState>(nullptr);
-
-					//std::cout << "Found final state !" << std::endl;
-
-					solution = FindSolution(map_itr, action_map, prev_ptr);
-					action_map.clear();				
-					return solution;
-				}
-			}
-			cost++;
-			state_ptr = nullptr;
-		}	
-	}
-	action_map.clear();
-	return {}; */
-
 	std::vector<SearchAction> solution;
-	std::priority_queue<std::pair<double, SearchState>> open;
+	std::priority_queue<std::pair<double, SearchState>,std::vector<std::pair<double,SearchState>>,compare> open;
 	std::map<SearchState, double> g_scores;
 	std::map<SearchState, double> f_scores;
 	std::set<SearchState> closed;
 	std::set<SearchState> open_checklist;
 	std::map<SearchState, std::tuple<std::shared_ptr<SearchState>, SearchAction> > action_map;
 	double cost = 0;	// g(n) part of the f(n) to calculate cost, ie. number of trips from source
-	bool tentative_is_better = false;
+	auto tentative_score = cost;
 
 	if (init_state.isFinal())
 		return {};
 
 	auto state_ptr = std::make_shared<SearchState>(init_state);
 	action_map.emplace(init_state, std::make_tuple(nullptr, init_state.actions()[0]));
-	open.emplace(std::make_pair(cost, init_state));
+	open.push(std::make_pair(cost, init_state));
 	open_checklist.insert(init_state);
 	g_scores.emplace(init_state, cost);
 	f_scores.emplace(init_state, compute_heuristic(init_state, *heuristic_));
@@ -289,12 +231,8 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
 		}
 
 		auto& top = open.top();
-		auto& a = top.second;
 
-		SearchState working_state(a);
-		open.pop();
-		open_checklist.erase(working_state);
-		closed.insert(working_state);
+		SearchState working_state(top.second);
 
 		if(working_state.isFinal()){
 			auto map_itr = action_map.find(working_state);
@@ -306,6 +244,10 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
 			return solution;
 		}
 		
+		open.pop();
+		open_checklist.erase(working_state);
+		closed.insert(working_state);
+
 		auto actions = working_state.actions();
 
 		for( auto &action : actions){
@@ -320,30 +262,23 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
 			
 			auto current_g = g_scores.find(working_state);
 			auto new_g = g_scores.find(new_state);
-			auto tentative_score = current_g->second + 1;
+			tentative_score = current_g->second + 1;
+
 			auto h_n = compute_heuristic(new_state, *heuristic_);
-			auto f_n = new_g->second + h_n;
+			auto f_n = tentative_score + h_n;
 
-			if (search_open == closed.end())
-			{
-				open.emplace(std::make_pair(f_n, new_state));
-				open_checklist.insert(new_state);
-				tentative_is_better = true;
-
+			if(tentative_score < new_g->second || new_g == g_scores.end()){
 				if (state_ptr == nullptr)
 					state_ptr = std::make_shared<SearchState>(working_state);
-			}
-			else if(tentative_score < new_g->second){
-				tentative_is_better = true;
-			}
-			else{
-				tentative_is_better = false;
-			}
 
-			if(tentative_is_better){
 				action_map.emplace(new_state, std::make_tuple(state_ptr, action));
 				g_scores.emplace(new_state, tentative_score);
 				f_scores.emplace(new_state, f_n);
+				
+				if(search_open == open_checklist.end()){
+					open.push(std::make_pair(f_n, new_state));
+					open_checklist.insert(new_state);
+				}
 			}
 		}
 		cost++;
